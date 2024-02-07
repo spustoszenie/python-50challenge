965988136bfff2047659f190870b0dbc59d22a63
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 23:15:49 +0100

    List of Prime Numbers

--- day08_extra.py
+++ day08_extra.py
@@ -1,3 +1,7 @@
+# List of Prime Numbers
+# Write a function called prime_numbers. This function asks a user to enter a number (integer) as an argument and returns a list of all the prime numbers within its range. 
+# For example, if user enters 10, your code should return [2, 3, 5, 7] as prime numbers.
+    
 def prime_numbers():
     liczba = int(input("Podaj cyferkę, lub liczbę : "))
     liczby_pierwsze = []
