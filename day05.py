df5361ffd7e7bc3d3693cd11fe68c41303624788
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 23:08:00 +0100

    My Discount

--- day05.py
+++ day05.py
@@ -1,3 +1,8 @@
+# My Discount
+# Create a function called my_discount. The function takes no arguments but asks the user to input the price and the discount (percentage) of the product. 
+# Once the user inputs the price and discount, it calculates the price after the discount.
+# The function should return the price after the discount. For example, if the user enters 150 as price and 15% as the discount, your function should return 127.5.
+
 def my_discount():
     cena = int(input("Podaje cenę: "))
     znizka = int(input("Podaj ile procent rabatu otrzymasz: "))
