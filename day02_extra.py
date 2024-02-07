d7eadfeb77a224b1aa9b7a6fd6e26e5d06df7671
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 23:00:29 +0100

    Duplicate Names

--- day02_extra.py
+++ day02_extra.py
@@ -1,3 +1,12 @@
+# Duplicate Names
+# Write a function called check_duplicates that takes a list of strings as an argument. The function should check if the list has any duplicates. 
+# If there are duplicates, the function should return the duplicates. If there are no duplicates, the function should return "No duplicates". 
+# For example, the list of fruits below should return apple as a duplicate and list of names should return "no duplicates".
+
+fruits = ['apple', 'orange', 'banana', 'apple']
+names = ['Yoda', 'Moses', 'Joshua', 'Mark']
+
+
 def check_duplicates(lista):
     duplicates = set()
     for i in lista:
@@ -8,10 +17,6 @@ def check_duplicates(lista):
         return duplicates
     else:
         return 'No duplicates'
-
-
-fruits = ['apple','orange','banana','apple']
-names = ['Yoda','Moses','Joshua','Mark']
 
 print(check_duplicates(fruits))
 print(check_duplicates(names))
