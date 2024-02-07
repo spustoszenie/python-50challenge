239035e5303ea2208d12ef0a0b332a839f577469
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 23:01:46 +0100

    Register Check

--- day03.py
+++ day03.py
@@ -1,10 +1,16 @@
+# Register Check
+# Write a function called register_check that checks how many students are in school. The function takes a dictionary as a parameter. 
+# If the student is in school, the dictionary says ‘yes’. If the student is not in school, the dictionary says ‘no’. 
+# Your function should return the number of students in school. Use the dictionary below. Your function should return 3.
+
+
+register = {'Michael':'yes','John': 'no', 'Peter':'yes', 'Mary': 'yes'}
+
 def register_check(register):
     present = 0
     for i in register.values():
         if i == 'yes':
             present += 1
     return present
-
-register = {'Michael':'yes', 'John':'no','Peter':'yes','Mary':'yes'}
 
 print(register_check(register))
