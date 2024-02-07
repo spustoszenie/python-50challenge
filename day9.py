7cc79648275c2a73ea3186224ce5630df70a3cef
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 22:37:22 +0100

    Delete day9.py

--- day9.py
+++
@@ -1,9 +0,0 @@
-def biggest_odd(string):
-    lista_ze_stringa = []
-    for i in string:
-        lista_ze_stringa.append(i)
-    max_z_listy = max(lista_ze_stringa)
-    return max_z_listy
-
-string = '23596'
-print(biggest_odd(string))
