5dda075d4522bbe61773e247fe0acbccd30ad348
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 22:59:24 +0100

    Strings to Integers

--- day02.py
+++ day02.py
@@ -1,3 +1,9 @@
+# Strings to Integers
+# Write a function called convert_add that takes a list of strings as an argument and converts it to integers and sums the list. 
+# For example [‘1’, ‘3’, ‘5’] should be converted to [1, 3, 5] and summed to 9
+
+list = ['1','3','5']
+
 def convert_add(lista):
     int_lista = []
     for i in lista:
@@ -6,8 +12,4 @@ def convert_add(lista):
     suma_listy = sum(int_lista)
     return suma_listy
 
-list = ['1','3','5']
-
 print(convert_add(list))
-
-
