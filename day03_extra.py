b7e2657b19da1bf4b3c9410088a270a54fb9da16
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 23:03:24 +0100

    Lowercase Names

--- day03_extra.py
+++ day03_extra.py
@@ -1,3 +1,9 @@
+# Lowercase Names
+# You are given a list of names above. This list is made up of names of lowercase and uppercase letters. 
+# Your task is to write a code that will return a tuple of all the names in the list that have only lowercase letters. 
+# Your tuple should have names sorted alphabetically in descending order. Using the list above, your code should return:
+# ('kerry', 'dickson', 'carol', 'adam')
+
 names = ['kerry','dickson','John','Mary','carol','Rose','adam']
 
 def print_lower_names(names):
