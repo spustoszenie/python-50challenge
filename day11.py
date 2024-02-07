60f2871ec61a1664b4fc0e059b40ec154b04ef0e
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 22:49:44 +0100

    Are They Equal?

--- day11.py
+++ day11.py
@@ -1,3 +1,8 @@
+# Are They Equal?
+# Write a function called equal_strings. The function takes two strings as arguments and compares them. 
+# If the strings are equal (if they have the same characters and have equal length), it should return True, if they are not, it should return False. 
+# For example, ‘love’ and ‘evol’ should return True.
+
 raz = 'love'
 lol = 'evol'
 trzy = 'loove'
