9c5e2e98a1e68d38faa35a2556774366850326b1
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 22:57:11 +0100

    Division and Square-root

--- day01.py
+++ day01.py
@@ -1,3 +1,7 @@
+# Division and Square-root
+# Write a function called divide_or_square that takes one argument (a number), and returns the square root of the number if it is divisible by 5, returns its remainder if it is not divisible by 5. 
+# For example, if you pass 10 as an argument, then your function should return 3.16 as the square root.
+
 import math
 def divide_or_square(x):
     y = math.sqrt(x)
