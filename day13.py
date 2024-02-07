3c3b916305ed8937bd8a7d5d2e40cc97f87512b7
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 22:53:10 +0100

    Pay Your Tax

--- day13.py
+++ day13.py
@@ -1,3 +1,9 @@
+# Pay Your Tax
+# Write a function called your_vat. The function takes no parameter. The function asks the user to input the price of an item and VAT (vat should be a percentage). 
+# The function should return the price of the item plus VAT. If the price is 220 and, VAT is 15% your code should return a vat inclusive price of 253.
+# Make sure that your code can handle ValueError. Ensure the code runs until valid numbers are entered. (hint: Your code should include a while loop).
+
+
 def your_vat():
     while True:
         try:
