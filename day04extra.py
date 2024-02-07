a03705d7225140a73b10be9227038858d6f3e85a
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 23:20:44 +0100

    Index of the Longest Word

--- day04extra.py
+++ day04extra.py
@@ -2,7 +2,6 @@
 # Write a function called word_index that takes one argument, a list of strings and returns the index of the longest word in the list. 
 # If there is no longest word (if all the strings are of the same length), the function should return zero (0).
 
-
 words1 = ["Hate", "remorse", "vengeance"]   # this list below should return 2.
 words2 = ["Love", "Hate"]                   # this list below, shoul return zero (0)
 
