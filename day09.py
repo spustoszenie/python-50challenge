110bebe568a5d03404917ca9c37a41ebed91b31b
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 23:17:15 +0100

    Biggest Odd Number

--- day09.py
+++ day09.py
@@ -1,3 +1,9 @@
+# Biggest Odd Number
+# Create a function called biggest_odd that takes a string of numbers and returns the biggest odd number in the list. 
+# For example, if you pass ‘23569’ as an argument, your function should return 9.
+
+string = '23596'
+
 def biggest_odd(string):
     lista_ze_stringa = []
     for i in string:
@@ -5,5 +11,4 @@ def biggest_odd(string):
     max_z_listy = max(lista_ze_stringa)
     return max_z_listy
 
-string = '23596'
 print(biggest_odd(string))
