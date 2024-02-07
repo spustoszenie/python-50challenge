607b709de8b303af1f1088fba9cabf8352703986
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 23:29:39 +0100

    Dictionary of names

--- day07_extra.py
+++ day07_extra.py
@@ -4,32 +4,6 @@
 # names = ["Joseph","Nathan", "Sasha", "Kelly","Muhammad", "Jabulani", "Sera”, "Patel", "Sera”]
 # Your code should return: {“Sasha”: 1, “Sera”: 2}
 
-
-names = ['Józio', 'Natka', 'Saszka', 'Kaja', 'Ali', 'Abdullah', 'Sara', 'Patelson', 'Sara']
-
-def wyluskanie_imion_na_dana_litere(X):
-    imiona = []
-    for i in names:
-        if i.startswith(X):
-            imiona.append(i)
-    return imiona
-
-def policzenie_powtorzen():
-    licznik_imion = {}
-    lista_imion_na_litere = wyluskanie_imion_na_dana_litere('s')
-
-    for i in lista_imion_na_litere:
-        if i in licznik_imion:
-            licznik_imion[i] += 1
-        else:
-            licznik_imion[i] = 1
-
-    return licznik_imion
-
-# Wywołanie funkcji i wydrukowanie wyniku
-wynik = policzenie_powtorzen(x)
-print(wynik)
-
 names = ['Józio', 'Natka', 'Sasha', 'Kaja', 'Ali', 'Abdullah', 'Sara', 'Patelson', 'Sara']
 
 def policzenie_powtorzen_dla_litery(X):
@@ -47,7 +21,3 @@ def policzenie_powtorzen_dla_litery(X):
 # Wywołanie funkcji i wydrukowanie wyniku
 wynik = policzenie_powtorzen_dla_litery('S')
 print(wynik)
-
-
-
-
