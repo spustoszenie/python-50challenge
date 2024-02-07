db507a02e6136de14526a4dcddf62e3a47f35d8b
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 22:51:18 +0100

    Swap Values

--- day11_extra.py
+++ day11_extra.py
@@ -1,3 +1,8 @@
+# Swap Values
+# Write a function called swap_values. This function takes a list of numbers and swaps the first element with the last element.
+# For example, if you pass [2, 4,67, 7] as a parameter, your function should return
+# [7, 4, 67, 2].
+
 xyz = [2,4,67,7]
 
 def swap_value(jakiestambyleco):
