f6e5798e3322c93cba1c513bf877ab6caaccac61
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 23:11:51 +0100

    Zero Both ends

--- day06_extra.py
+++ day06_extra.py
@@ -1,3 +1,7 @@
+# Zero Both ends
+# Write a function called zeroed code that takes a list of numbers as an argument. The code should zero (0) the first and the last number in the list. 
+# For example, if the input is [2, 5, 7, 8, 9], your code should return [0, 5, 7, 8, 0].
+
 lista1 = [2,5,7,8,9]
 
 def zeroed(lista_cyferek):
