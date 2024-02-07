18eca82a0d08aaab5d4d9be8122263623ba406db
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 22:46:19 +0100

    Hide my Password
    Hide my Password

--- day10.py
+++ day10.py
@@ -1,4 +1,4 @@
-# Hide my Password
+# Hide my Password extra
 
 # Write a function called hide_password that takes no parameters. The function takes an input (a password) from a user and returns a hidden password. 
 # For example, if the user enters ‘hello’ as a password the function should return ‘****’ as a password and tell the user that the password is 4 characters long.
