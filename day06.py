95527d8b2d441bcb5fd83eb333dae549cbe5ad61
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 23:11:05 +0100

    User Name Generator
    User Name Generator

--- day06.py
+++ day06.py
@@ -1,3 +1,8 @@
+# User Name Generator
+# Write a function called user_name that generates a username from the user’s email. 
+# The code should ask the user to input an email and the code should return everything before the @ sign as their user name. 
+# For example, if someone entersben@gmail.com, the code should return ben as their username.
+
 def user_name():
     email = input("Podaj mi no tu adresik e-mail: ")
     username = email.split('@')
