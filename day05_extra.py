69e0450a8996b3bc3f95ddf6d31b3bb8d184f589
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 23:09:55 +0100

    Tuple of Student Sex

--- day05_extra.py
+++ day05_extra.py
@@ -1,4 +1,12 @@
+# Tuple of Student Sex
+# You work for a school and your boss wants to know how many female and male students are enrolled in the school. 
+# The school has saved the students in a list. Your task is to write a code that will count how many males and females are in the list. 
+# Your code should return a list of tuples. The list above should return:
+# [(‘Male’,7), (‘female’,6)]
 
+
+students = ['Male', 'Female', 'female', 'male', 'male', 'male','female', 'male', 'Female', 'Male', 'Female', 'Male', 'female']
+        
 def obecnosc(x):
     boys = 0
     girls = 0
@@ -15,5 +23,4 @@ def obecnosc(x):
     macie_tuple = [('Male', boys), ('Female', girls)]
     print(macie_tuple)
     return macie_tuple
-students = ['Male','Female','Female','male','Male','Female','Female','male','Male','Female','male','male','Female']
 print(obecnosc(students))
