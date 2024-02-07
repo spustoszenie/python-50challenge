9bdf63c1ebee10fe8fcc0d524925b570d7387b92
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-07 22:48:23 +0100

    A Thousand Separator

--- day10_extra.py
+++ day10_extra.py
@@ -1,3 +1,13 @@
+# A Thousand Separator
+
+# Your new company has a list of figures saved in a list. The issue is that these numbers have no separator. The numbers are saved in the following format:
+# [1000000, 2356989, 2354672, 9878098].
+# You have been asked to write a code that will convert each of the numbers in the list into a string. Your code should then add a comma on each number as a thousand separator for readability. 
+# When you run your code on the above list, your output should be:
+# [ '1,000,000', '2,356,989', '2,354,672', '9,878,098’]
+# Write a function called convert_numbers that will take one argument, a list of numbers above.
+
+
 po_tysiackroc = ['1000000', '2356989', '245158', '65958751']
 
 def convert_number(byleco):
