4b8a082e3ecb7b99de05f9bcf3534d697b8cc564
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-08 16:56:46 +0100

    Flatten the list.

---
+++ day14.py
@@ -0,0 +1,15 @@
+# Flatten the List
+# Write a function called flat_list that takes one argument, a nested list. 
+# The function converts the nested list into a one-dimension list. For example [[2,4,5,6]] should return [2,4,5,6]
+
+
+def flat_list(zagniezdzona):
+    final_list = []
+    for i in zagniezdzona:
+        for j in i:
+            final_list.append(j)
+
+    return final_list
+
+nested_list = [[2,4,5,6]]
+print(flat_list(nested_list))
