dc393b1c7126e6aeca4ac8dc4f3159acc17f7d3b
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-11 19:52:04 +0100

    Pyramid of Snakes

--- day13_extra.py
+++ day13_extra.py
@@ -1,10 +1,17 @@
 # Pyramid of Snakes
 # Write a function called Python_snakes that takes a number as an argument and creates the following shape, using the number’s range: (hint: Use the loops and emoji library. 
-# If you pass 4 as argument, your code should print the following
-#     🐍
-#    🐍🐍
-#   🐍🐍🐍
+# If you pass 7 as argument, your code should print the following
+ #      🐍
 
+ #     🐍 🐍
+
+ #    🐍 🐍 🐍
+
+ #   🐍 🐍 🐍 🐍
+
+ #  🐍 🐍 🐍 🐍 🐍
+
+ # 🐍 🐍 🐍 🐍 🐍 🐍
 
 from emoji import emojize
 
