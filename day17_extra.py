2f81a8279780444726f73f630f257b610ff0bed0
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-15 22:34:56 +0100

    Sort by Length

--- day17_extra.py
+++ day17_extra.py
@@ -12,3 +12,35 @@ def sort_length(lista_stringow :list):
     return sort
 
 print(sort_length(names))
+
+############ ZAKAZ FUNKCJI SORT!!!!!!!! #############
+
+names = [ "Peter", "Jon", "Andrew"]
+
+def sort_length(lista_stringow :list):
+    len1 = []
+    len2 = []
+    len3 = []
+    len4 = []
+    len5 = []
+    len6 = []
+    len7 = []
+    for i in lista_stringow:
+        if len(i) == 1:
+            len1.append(i)
+        elif len(i) == 2:
+            len2.append(i)
+        elif len(i) == 3:
+            len3.append(i)
+        elif len(i) == 4:
+            len4.append(i)
+        elif len(i) == 5:
+            len5.append(i)
+        elif len(i) == 6:
+            len6.append(i)
+        elif len(i) == 7:
+            len7.append(i)
+    posortowana = len1 + len2 + len3 + len4 + len5 + len6 + len7
+    return posortowana
+  
+print(sort_length(names))
