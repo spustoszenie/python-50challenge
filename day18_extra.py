73bbcf3285148813f87c3565cae5398758992bc7
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-17 06:45:45 +0100

    Add and reverse

---
+++ day18_extra.py
@@ -0,0 +1,4 @@
+# Add and Reverse
+# Write a function called add_reverse. This function takes two lists as argument and adds each corresponding number, and reverses the outcome. 
+# For example, if you pass [10, 12, 34] and [12, 56, 78]. Your code should return [112, 22, 68]. 
+# If the two lists are not of equal lengths, the code should return a message that "the lists are not of equal lengths".
