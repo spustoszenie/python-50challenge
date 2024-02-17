6b791af39f2e7021e92c7727cb900c6a7a230372
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-17 06:10:37 +0100

    Simple calculator

---
+++ day23.py
@@ -0,0 +1,3 @@
+# Simple Calculator
+# Create a simple calculator. The calculator should be able to perform basic math operations, add, subtract, divide and multiply. 
+# The calculator should take input from users. The calculator should be able to handle ZeroDivisionError, NameError, and ValueError.
