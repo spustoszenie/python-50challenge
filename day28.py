e9bace10cfe917e08aec3cf9da6c3fe5ef75870a
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-24 21:25:31 +0100

    Return Indexes

--- day28.py
+++ day28.py
@@ -20,14 +20,11 @@ print(index_position("LovE"))
 
 #### Wersja z enumerate:
 
-def index_position(jakis_string: str):
-    low_letters = []
-    indeksiki = []
-    for idx, char in enumerate(jakis_string):
-        if char.islower():
-            low_letters.append(char)
-        indeksiki.append(idx)
-    print("Low letters:", low_letters)  # Drukuj low_letters po zakończeniu pętli
-    return indeksiki
-
-print(index_position("LovE"))
+def index_position (a):
+    idex = []
+    for i, item in enumerate(a):
+        if item.islower():
+        idex.append(i)
+    return idex
+    
+print(index_position('LovE'))
