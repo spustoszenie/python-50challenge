899ea077f4ddd7d415f4f200e5a8ae7431a4655d
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-24 18:35:16 +0100

    Create day28.py

---
+++ day28.py
@@ -0,0 +1,18 @@
+# Return Indexes
+# Write a function called index_position. 
+# This function takes a string as a parameter and returns the positions or indexes of all lower letters in the string. 
+# For example, ‘LovE’ should return [1,2].
+
+def index_position(jakis_string: str):
+    low_letters = []
+    indeksiki = []
+    for i in jakis_string:
+        if i.islower():
+            low_letters.append(i)
+
+    for i in range(len(jakis_string)):
+        if jakis_string[i] in low_letters:
+            indeksiki.append(i)
+    return indeksiki
+    
+print(index_position("LovE"))
