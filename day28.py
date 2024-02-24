096c7c07bc5183d6bcccb35d25412758e187f81d
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-24 18:36:34 +0100

    Return Indexes

--- day28.py
+++ day28.py
@@ -16,3 +16,18 @@ def index_position(jakis_string: str):
     return indeksiki
     
 print(index_position("LovE"))
+
+
+#### Wersja z enumerate:
+
+def index_position(jakis_string: str):
+    low_letters = []
+    indeksiki = []
+    for idx, char in enumerate(jakis_string):
+        if char.islower():
+            low_letters.append(char)
+        indeksiki.append(idx)
+    print("Low letters:", low_letters)  # Drukuj low_letters po zakończeniu pętli
+    return indeksiki
+
+print(index_position("LovE"))
