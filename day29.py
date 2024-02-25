c1052a5cd6ef4016a223ba496e93e20e0d84cebb
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-25 19:22:46 +0100

    Middle Figure

---
+++ day29.py
@@ -0,0 +1,19 @@
+# Middle Figure
+# Write a function called middle_figure that takes two parameters a, and b. The two parameters are strings. 
+# The function joins the two strings and finds the middle element.
+# If the combined string has a middle element, the function should return the element, otherwise, return ‘no middle figure’. 
+# Use ‘make love’ as an argument for a and ‘not wars’ as an argument for b. 
+# Your function should return ‘e’ as the middle element. Whitespaces should be removed.
+
+
+abc = "make love"
+xyz = "not wars"
+def middle_figgure(a:str, b:str):
+    joined = a+b
+    if len(joined) / 2 == 0:
+        print(" no middle figure")
+    else:
+        srodkowy = int(len(joined) / 2 - 0.5)
+        return joined[srodkowy]
+
+print(middle_figgure(abc, xyz))
