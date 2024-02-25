dbf4ddec07092ed4994e276567dcce168f675f29
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-02-25 19:27:51 +0100

    Update day29.py

--- day29.py
+++ day29.py
@@ -5,15 +5,15 @@
 # Use ‘make love’ as an argument for a and ‘not wars’ as an argument for b. 
 # Your function should return ‘e’ as the middle element. Whitespaces should be removed.
 
-
 abc = "make love"
 xyz = "not wars"
 def middle_figgure(a:str, b:str):
     joined = a+b
-    if len(joined) / 2 == 0:
+    bez_spacji = joined.replace(' ', '')
+    if len(bez_spacji) / 2 == 0:
         print(" no middle figure")
     else:
-        srodkowy = int(len(joined) / 2 - 0.5)
-        return joined[srodkowy]
+        srodkowy = int(len(bez_spacji) / 2 - 0.5)
+        return bez_spacji[srodkowy]
 
 print(middle_figgure(abc, xyz))
