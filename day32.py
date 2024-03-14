d7841ab6cdb0bbb97d30f9c6316ad78fca49da14
spustoszenie <159361953+spustoszenie@users.noreply.github.com>
2024-03-14 11:43:06 +0100

    Password Validator

--- day32.py
+++ day32.py
@@ -1,3 +1,11 @@
+# Password Validator
+# Write a function called password_validator. The function asks the user to enter a password. 
+# A valid password should have at least one upper letter, one lower letter, and one number. 
+# It should not be less than 8 characters long. When the user enters a password, the function should check if the password is valid. 
+# If the password is valid, the function should return the valid password. 
+# If the password is not valid, the function should tell the users the errors in the password and prompt the user to enter another password. 
+# The code should only stop once the user enters a valid password. (use while loop).
+
 def password_validator():
     
     while True:
